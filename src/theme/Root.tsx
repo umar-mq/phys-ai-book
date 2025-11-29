7d4566f37e961b01aeffdbfd14c6f9a37135ee98
umarmnaq <102142660+umarmnaq@users.noreply.github.com>
2025-11-29 19:04:57 +0500

    feat(speckit): complete T006 - wrap root with PersonalizationProvider

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ChatWidget from '../components/ChatWidget';
+import PersonalizationProvider from '../contexts/PersonalizationProvider';
 
 // Default implementation, that you can customize
 export default function Root({children}) {
   return (
-    <>
+    <PersonalizationProvider>
       {children}
       <ChatWidget />
-    </>
+    </PersonalizationProvider>
   );
 }
